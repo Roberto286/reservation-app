93d8bf01f76d137fac768735ba30c560f0402b39
Roberto Saliola <roberto.saliola96@gmail.com>
2025-11-26 22:17:44 +0100

    feat: add environment configuration for development and update app configuration

---
+++ packages/fe/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  beBaseUrl: 'http://localhost:3000',
+  production: false,
+};
---
+++ packages/fe/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  beBaseUrl: '',
+  production: false,
+};
