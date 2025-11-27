1520967213805f5a50b71f2520c21abd0829b375
Roberto Saliola <roberto.saliola96@gmail.com>
2025-11-27 22:47:50 +0100

    feat(fe): update login button component to use dynamic class binding and import CommonModule

--- packages/fe/src/app/components/login-button.ts
+++ packages/fe/src/app/components/login-button.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { AuthStateService } from '../core/services/auth-state.service';
 
 @Component({
   selector: 'app-login-button',
-  imports: [],
-  template: ` <a class="btn btn-primary" [href]="isAuthenticated() ? '/logout' : '/login'">
+  imports: [CommonModule],
+  template: ` <a
+    class="btn"
+    [ngClass]="{ 'btn-primary': !isAuthenticated(), 'btn-secondary': isAuthenticated() }"
+    [href]="isAuthenticated() ? '/logout' : '/login'"
+  >
     @if(isAuthenticated()){Logout}@else{Login}
   </a>`,
   styles: [],
