90e77a0900d476f795a992fc4e8371eadfb3893c
Roberto Saliola <roberto.saliola96@gmail.com>
2025-11-28 20:10:31 +0100

    feat(validation): add global validation pipe with whitelist and transform options

--- packages/be/src/main.ts
+++ packages/be/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
@@ -5,6 +6,13 @@ const PORT = process.env.PORT ?? 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
   await app.listen(PORT);
 }
 bootstrap().then(() => console.log(`Server is running on port ${PORT}`));
