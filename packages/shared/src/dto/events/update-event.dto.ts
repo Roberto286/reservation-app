f6afe3fd8417e1ebda7aad9ad17529766a101ec6
Roberto Saliola <roberto.saliola96@gmail.com>
2025-11-30 16:01:15 +0100

    feat(update-event-dto): redefine UpdateEventDto with detailed validation rules

--- packages/shared/src/dto/events/update-event.dto.ts
+++ packages/shared/src/dto/events/update-event.dto.ts
@@ -1,4 +1,46 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateEventDto } from "./create-event.dto";
+import { Type } from "class-transformer";
+import {
+  IsDate,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from "class-validator";
+import { FutureDate } from "class-validator-extended";
+import { EventCategory } from "../../event-category";
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @Type(() => Date)
+  @IsDate()
+  @FutureDate()
+  startAt?: Date;
+
+  @IsOptional()
+  @Type(() => Date)
+  @IsDate()
+  @FutureDate()
+  endAt?: Date;
+
+  @IsOptional()
+  @IsString()
+  location?: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  maxParticipants?: number;
+
+  @IsOptional()
+  @IsEnum(EventCategory)
+  category?: EventCategory;
+}
