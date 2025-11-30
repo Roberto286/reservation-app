d3a75daa49d548ccc61d9a5130b5855e53ae80bc
Roberto Saliola <roberto.saliola96@gmail.com>
2025-11-30 12:38:12 +0100

    feat(reservations): implement reservations page with data fetching and display logic

--- packages/fe/src/app/pages/reservations/reservations.ts
+++ packages/fe/src/app/pages/reservations/reservations.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { GetBookingDto } from '@reservation-app/shared';
+import { ReservationCard } from '../../components/reservation-card/reservation-card';
+import { AuthStateService } from '../../core/services/auth-state.service';
 
 @Component({
   selector: 'app-reservations',
-  imports: [],
+  imports: [ReservationCard],
   templateUrl: './reservations.html',
   styleUrl: './reservations.css',
 })
 export class Reservations {
+  private readonly http = inject(HttpClient);
+  private readonly authService = inject(AuthStateService);
+  private readonly userId = this.authService.getUserId();
+  protected reservations = signal<GetBookingDto[]>([]);
+  constructor() {
+    this.fetchReservations();
+  }
 
+  private fetchReservations() {
+    this.http.get<GetBookingDto[]>(`/bookings/${this.userId}`).subscribe((data) => {
+      this.reservations.set(data);
+    });
+  }
 }
