db860ee8d0277f8d43b9a8586d48f7ce994c1966
Roberto Saliola <roberto.saliola96@gmail.com>
2025-11-30 12:54:46 +0100

    feat(reservation-card): enhance cancellation process with modal confirmation and refactor button components

--- packages/fe/src/app/components/reservation-card/reservation-card.ts
+++ packages/fe/src/app/components/reservation-card/reservation-card.ts
@@ -1,13 +1,20 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { GetBookingDto } from '@reservation-app/shared';
+import { Button } from '../button/button';
+import { Modal } from '../modal/modal';
 
 @Component({
   selector: 'app-reservation-card',
-  imports: [DatePipe, CommonModule],
+  imports: [DatePipe, CommonModule, Button, Modal],
   templateUrl: './reservation-card.html',
   styleUrl: './reservation-card.css',
 })
 export class ReservationCard {
   reservationData = input.required<GetBookingDto>();
+  openModal = signal(false);
+
+  onCancel() {
+    this.openModal.set(true);
+  }
 }
